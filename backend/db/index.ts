695cd761391571b9a4596648d10b504109669f23
AleksanderGPL <me@aleksander.cc>
2025-05-09 18:29:53 +0200

    Add drizzle ORM to backend + users table

---
+++ backend/db/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema.ts";
+
+if (!Deno.env.get("DB_URL")) {
+  throw new Error("DB_URL is not set");
+}
+
+export const db = drizzle(Deno.env.get("DB_URL")!, { schema });

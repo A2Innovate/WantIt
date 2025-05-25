64a6894cd1706a007f75e3d55295c36764081b3f
AleksanderGPL <me@aleksander.cc>
2025-05-25 20:45:06 +0200

    Create function for formatting time, Improve design of Chat page

--- frontend/pages/user/chat/index.vue
+++ frontend/pages/user/chat/index.vue
@@ -7,9 +7,28 @@
           <UiCard
             v-for="message in messageStore.lastMessages"
             :key="message.sender.id"
+            class="hover:bg-neutral-800 transition-colors cursor-pointer"
           >
-            <p>{{ message.sender.name }}</p>
-            <p>{{ message.content }}</p>
+            <div class="flex items-center gap-4">
+              <div
+                class="w-12 h-12 rounded-full bg-neutral-700 flex items-center justify-center text-xl font-medium"
+              >
+                {{ message.sender.name.charAt(0).toUpperCase() }}
+              </div>
+              <div class="flex-1 min-w-0">
+                <div class="flex items-center justify-between">
+                  <h3 class="font-medium truncate">
+                    {{ message.sender.name }}
+                  </h3>
+                  <span class="text-sm text-neutral-400">
+                    {{ formatTime(new Date(message.createdAt)) }}
+                  </span>
+                </div>
+                <p class="text-sm text-neutral-400 truncate">
+                  {{ message.content }}
+                </p>
+              </div>
+            </div>
           </UiCard>
         </div>
       </UiCard>

a12a80cf592e25418f2136783556d155b3004514
AleksanderGPL <me@aleksander.cc>
2025-05-27 11:14:05 +0200

    Add general confirmation modal

---
+++ frontend/components/Modal/Confirm.vue
@@ -0,0 +1,24 @@
+<template>
+  <UiModal :is-open="isOpen" card-class="sm:min-w-md" @close="emit('cancel')">
+    <div class="flex flex-col items-center gap-4">
+      <Icon
+        name="material-symbols:warning-rounded"
+        size="3rem"
+        class="text-orange-400"
+      />
+      <slot />
+      <div class="flex w-full justify-end gap-2">
+        <UiButton @click="emit('cancel')">Cancel</UiButton>
+        <UiButton @click="emit('confirm')">Confirm</UiButton>
+      </div>
+    </div>
+  </UiModal>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  isOpen: boolean;
+}>();
+
+const emit = defineEmits(['cancel', 'confirm']);
+</script>
